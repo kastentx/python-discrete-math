eb3bc73549dd047a64486fa044d1e70b075fd857
Nick Kasten <kastencode@gmail.com>
2017-04-09 00:54:44 -0500

    implemented some graph functions

--- graphStuff.py
+++ graphStuff.py
@@ -4,14 +4,22 @@ import sys
 import itertools
 
 # Load the adjacency matrix
-G = np.loadtxt("Paw.txt", int)
-print("The adjacency matrix of G is: ")
-print(G)
-
+G = np.loadtxt("graphs/graph3.txt", int)
 
 def order(G):
-    n = len(G)
-    return n
+    return len(G)
+
+def size(G):
+    return int(np.sum(G) / 2)
+
+def degree(G,v):
+    return np.sum(G[v])
+
+def maxDegree(G):
+    return np.amax(np.sum(G, axis=1))
+
+def minDegree(G):
+    return np.amin(np.sum(G, axis=1))
 
 def openNeighborhood(G,v):
     neighborhood = set()
@@ -20,10 +28,17 @@ def openNeighborhood(G,v):
             neighborhood.add(i)
     return neighborhood
 
-def degree(G,v):
-    d = len(openNeighborhood(G,v))
-    return d
+
 
 # Function Calls
-print(openNeighborhood(G,1))
-print(degree(G,1))
+print("The adjacency matrix of G is: ")
+print(G)
+print('\n')
+print('order of G:', order(G))
+print('size:', size(G))
+print('max degree:', maxDegree(G))
+print('min degree:', minDegree(G))
+print('\n')
+for i in range(0,order(G)):
+    print('degree of %s:' % i, degree(G,i))
+    print('open neighborhood of %s:' % i, openNeighborhood(G,i))
